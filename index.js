012dbbcd055b1e8961d6807064ca9038c19dc6da
WebhookShout <antihttpspywebhookshout@gmail.com>
2025-07-26 00:19:10 +0800

    Update index.js

--- index.js
+++ index.js
@@ -126,7 +126,7 @@ export default {
       const linkData = links[key];
       const data = JSON.parse(DecodeText(auth, ServiceKey));
       
-      if (!linkData || !data) {
+      if (!linkData) {
         return new Response(`404: Not Found`, { status: 404 });
       }
 
