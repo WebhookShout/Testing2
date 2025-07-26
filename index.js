bdeea755756bb9c4c010c2ac77d1c340661921fc
WebhookShout <antihttpspywebhookshout@gmail.com>
2025-07-26 12:12:35 +0800

    Update index.js

--- index.js
+++ index.js
@@ -151,9 +151,9 @@ export default {
       const decodedStr = GetRandomName();
       const fnStr = GetRandomName();
       const script = `
-      function ${decodedStr}(encodedStr, key) local result = {} local parts = string.split(encodedStr, "/") for i = 1, #parts do local byte = tonumber(parts[i]) local k = key:byte(((i - 1) % #key) + 1) local decoded = (byte - k + 256) % 256 table.insert(result, string.char(decoded)) end return table.concat(result) end 
-      local a = game local b = "GetService" local c = "ReplicatedStorage" local d = "Destroy" obj = a[b](a, c)["${data.Name}"] 
-      local ${fnStr}="";for _, c in ipairs({108, 111, 97, 100, 115, 116, 114, 105, 110, 103}) do ${fnStr}=${fnStr}..string.char(c);end(getfenv()[${fnStr}] or _G[${fnStr}] or _ENV and _ENV[${fnStr}])('${decodedStr}("${encoded}", obj.Value)')()`;
+      local function ${decodedStr}(encodedStr, key) local result = {} local parts = string.split(encodedStr, "/") for i = 1, #parts do local byte = tonumber(parts[i]) local k = key:byte(((i - 1) % #key) + 1) local decoded = (byte - k + 256) % 256 table.insert(result, string.char(decoded)) end return table.concat(result) end 
+      local a = game local b = "GetService" local c = "ReplicatedStorage" local d = "Destroy" local obj = a[b](a, c)["${data.Name}"].Value 
+      local ${fnStr}="";for _, c in ipairs({108, 111, 97, 100, 115, 116, 114, 105, 110, 103}) do ${fnStr}=${fnStr}..string.char(c);end(getfenv()[${fnStr}] or _G[${fnStr}] or _ENV and _ENV[${fnStr}])(${decodedStr}("${encoded}", obj))()`;
       
       return new Response(script, {
         headers: { "Content-Type": "text/plain" }
