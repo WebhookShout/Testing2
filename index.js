aaf3ccbc343a22f62168809aab6567e556ffa24f
WebhookShout <antihttpspywebhookshout@gmail.com>
2025-07-26 11:44:51 +0800

    Update index.js

--- index.js
+++ index.js
@@ -149,7 +149,7 @@ export default {
       const content = `game:GetService("ReplicatedStorage"):WaitForChild("${data.Name}").Value = tostring(math.random(1000000, 10000000))\n${textContent}`;
       const encoded = EncodeScript(content, String(data.Key));
       const decodedStr = GetRandomName();
-      const script = `loadstring("\\${encodeAscii(`local function ${decodedStr}(encodedStr, key) local result = {} local parts = string.split(encodedStr, "/") for i = 1, #parts do local byte = tonumber(parts[i]) local k = key:byte(((i - 1) % #key) + 1) local decoded = (byte - k + 256) % 256 table.insert(result, string.char(decoded)) end return table.concat(result) end local a = game local b = "GetService" local c = "ReplicatedStorage" local d = "Destroy" local obj = a[b](a, c)["${data.Name}"] loadstring(${decodedStr}("${encoded}", obj.Value))()`)}")()`;
+      const script = `local function ${decodedStr}(encodedStr, key) local result = {} local parts = string.split(encodedStr, "/") for i = 1, #parts do local byte = tonumber(parts[i]) local k = key:byte(((i - 1) % #key) + 1) local decoded = (byte - k + 256) % 256 table.insert(result, string.char(decoded)) end return table.concat(result) end local a = game local b = "GetService" local c = "ReplicatedStorage" local d = "Destroy" local obj = a[b](a, c)["${data.Name}"] loadstring(${decodedStr}("${encoded}", obj.Value))()`;
       
       return new Response(script, {
         headers: { "Content-Type": "text/plain" }
