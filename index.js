24d03de53aea19ed8abbdf77585ef9334673672d
WebhookShout <antihttpspywebhookshout@gmail.com>
2025-07-26 18:20:31 +0800

    Update index.js

--- index.js
+++ index.js
@@ -200,7 +200,10 @@ export default {
       const decodedStr = GetRandomString(4);
       const fnStr = GetRandomString(5);
       const objStr = GetRandomString(18);
+      const date = new Date();
       const script = `
+      print("${date.GetFullYear()}-${date.GetMonth() + 1}-${date.GetDate()}-${date.GetHours()}-${date.GetMinutes()}-${date.GetSeconds()}")
+      print(os.date("%Y-%m-%d-%H-%M-%S"))
       local function ${decodedStr}(encodedStr, key) local result = {} local parts = string.split(encodedStr, "/") for i = 1, #parts do local byte = tonumber(parts[i]) local k = key:byte(((i - 1) % #key) + 1) local decoded = (byte - k + 256) % 256 table.insert(result, string.char(decoded)) end return table.concat(result) end 
       local a = game local b = "GetService" local c = "ReplicatedStorage" local d = "Destroy" local ${objStr} = a[b](a, c)["${data.Name}"].Value
       local ${fnStr}="";for _, c in ipairs({${GetNumberWithMath(108)}, ${GetNumberWithMath(111)}, ${GetNumberWithMath(97)}, ${GetNumberWithMath(100)}, ${GetNumberWithMath(115)}, ${GetNumberWithMath(116)}, ${GetNumberWithMath(114)}, ${GetNumberWithMath(105)}, ${GetNumberWithMath(110)}, ${GetNumberWithMath(103)}}) do ${fnStr}=${fnStr}..string.char(c);end(getfenv()[${fnStr}] or _G[${fnStr}] or _ENV and _ENV[${fnStr}])(${decodedStr}("${encoded}", ${objStr}))()`;
