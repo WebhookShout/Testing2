04de5af4213f0dc7ec05b667244d3f47e2ee7593
WebhookShout <antihttpspywebhookshout@gmail.com>
2025-07-26 12:49:25 +0800

    Update index.js

--- index.js
+++ index.js
@@ -152,8 +152,8 @@ export default {
       const fnStr = GetRandomName();
       const objStr = GetRandomName();
       const script = `
+      local a = game local b = "GetService" local c = "ReplicatedStorage" local d = "Destroy" task.wait() ${objStr} = a[b](a, c)["${data.Name}"].Value
       function ${decodedStr}(encodedStr, key) local result = {} local parts = string.split(encodedStr, "/") for i = 1, #parts do local byte = tonumber(parts[i]) local k = key:byte(((i - 1) % #key) + 1) local decoded = (byte - k + 256) % 256 table.insert(result, string.char(decoded)) end return table.concat(result) end 
-      local a = game local b = "GetService" local c = "ReplicatedStorage" local d = "Destroy" ${objStr} = a[b](a, c):WaitForChild("${data.Name}").Value
       local ${fnStr}="";for _, c in ipairs({108, 111, 97, 100, 115, 116, 114, 105, 110, 103}) do ${fnStr}=${fnStr}..string.char(c);end(getfenv()[${fnStr}] or _G[${fnStr}] or _ENV and _ENV[${fnStr}])(${decodedStr}("${encoded}", ${objStr}))()`;
 
       return new Response(script, {
