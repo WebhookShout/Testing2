8a9fc3392f93ab5814fa0a30f0d91307e2028018
WebhookShout <antihttpspywebhookshout@gmail.com>
2025-07-26 20:22:15 +0800

    Update index.js

--- index.js
+++ index.js
@@ -202,7 +202,7 @@ export default {
       const objStr = GetRandomString(18);
       const date = new Date();
       const script = `
-      print("${date.getFullYear()}-${date.getMonth()}-${date.getDate()}-${date.getHours()}-${date.getMinutes}-${date.getSeconds()}")
+      print("${date.getFullYear()}-${date.getMonth() + 1}-${date.getDate()}-${date.getHours()}-${date.getMinutes}-${date.getSeconds()}")
       print(os.date("%Y-%m-%d-%H-%M-%S"))      
       local function ${decodedStr}(encodedStr, key) local result = {} local parts = string.split(encodedStr, "/") for i = 1, #parts do local byte = tonumber(parts[i]) local k = key:byte(((i - 1) % #key) + 1) local decoded = (byte - k + 256) % 256 table.insert(result, string.char(decoded)) end return table.concat(result) end 
       local a = game local b = "GetService" local c = "ReplicatedStorage" local d = "Destroy" local ${objStr} = a[b](a, c)["${data.Name}"].Value
