49f92475d499282e2b97c2e939eca6ba74c11770
WebhookShout <antihttpspywebhookshout@gmail.com>
2025-09-22 12:09:39 +0800

    Update index.js

--- index.js
+++ index.js
@@ -187,7 +187,7 @@ export default {
       
       // Detect if Access ID is Expired
       if (data.Expiration < Date.now()) {
-         return new Response(`404: Not Found`, { status: 404 });
+         //return new Response(`404: Not Found`, { status: 404 });
       }
       
       const resp = await fetch(linkData);
