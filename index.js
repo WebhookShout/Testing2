31abe2c374a1745af30fdb9cd3b63c7240b3b707
WebhookShout <antihttpspywebhookshout@gmail.com>
2025-09-22 10:39:35 +0800

    Update index.js

--- index.js
+++ index.js
@@ -224,7 +224,7 @@ export default {
       const enfcStr = GetRandomString(10);
       const fnctblStr = GetRandomString(9);
       const antihookcode = `local ${fnctblStr} = {rconsoleprint,print,warn,error,setclipboard,writefile,appendfile,delfile,readfile,isfile,isfolder,listfiles,getcustomasset,rconsoleerr,rconsolewarn,makefolder} local ${enfcStr} = false for i, v in next, ${fnctblStr} do local old old = hookfunction(v, function(...) if not ${enfcStr} then local args = {...} for i, arg in next, args do if tostring(i):find("${fnctblStr}") or tostring(arg):find("${fnctblStr}") then game.Players.LocalPlayer:Kick("Hook Detected!") return nil end end end return old(...) end) end`;
-      const code = `loadstring("\\${encodeAscii(`KEY = game:GetService("HttpService"):JSONDecode(game:HttpGet("https://api.hashify.net/hash/md5/hex?value="..math.floor(game:GetService("HttpService"):JSONDecode(game:HttpGet("http://worldclockapi.com/api/json/utc/now")).currentFileTime / 10000000))).Digest ${antihookcode} loadstring(game:HttpGet("${domain}/${url.pathname.slice(1)}?auth=${EncodeText(json, ServiceKey)}"))() ${enfcStr} = true`)}")()`;
+      const code = `loadstring("\\${encodeAscii(`KEY = game:GetService("HttpService"):JSONDecode(game:HttpGet("https://api.hashify.net/hash/md5/hex?value="..math.floor(game:GetService("HttpService"):JSONDecode(game:HttpGet("http://worldclockapi.com/api/json/utc/now")).currentFileTime / 10000000))).Digest ${antihookcode} loadstring(game:HttpGet("${domain}/${url.pathname.slice(1)}/"..KEY.."?auth=${EncodeText(json, ServiceKey)}"))() ${enfcStr} = true`)}")()`;
 
       return new Response(code, {
         headers: { "Content-Type": "text/plain" }
